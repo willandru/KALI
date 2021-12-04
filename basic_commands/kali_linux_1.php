63ae9b869f1919d325e19853f8e88b2bf79792ca
willandru <willandru@hotmail.com>
2021-12-04 09:26:43 -0500

    Set Up files to LEARN basics of KALI LINUX :x

---
+++ basic_commands/kali_linux_1.php
@@ -0,0 +1,16 @@
+COMANDOS QUE APRENDEREMOS::
+~~~~~~~~~~~~~~~~~~~~~
+pwd
+cd
+ls
+mkdir, rmdir
+cd, rm, mv, locate
+updatedb
+passwd
+man
+~~~~~~~~~~~~~~~~~~
+
+paswwd --> change root password
+
+man ls
+ls --help
